5d371bfb58dfe3a870d3c37130bfa004d5c18331
Henry Zhang <henryzhang9802@gmail.com>
2019-06-13 19:15:13 +0100

    Automatic change

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 var green = "linear-gradient(to right, #dce35b, #45b649)"
+var darkGreen = "linear-gradient(to right, #A8E349, #569E20)"
+var orange = "linear-gradient(to right, #FED524, #F95A57)"
+var red = "linear-gradient(to right, #C63EC6, #E00B0B)"
 
-
-
-setTimeout(typeWriter, 1000);
-function typeWriter() {
-    document.getElementById("sc").style.width="50%";
-}
 
 var studentCentre = new progressBar();
 studentCentre.create("sc", "Student Centre")
+
+setTimeout(typeWriter, 1000);
+function typeWriter() {
+    studentCentre.updateContent(50)
+}
 
 /*
 Example Div:
@@ -31,10 +33,10 @@ function progressBar(){
         this.placeName = name;
         iDiv.className = 'progress-wrapper progress-loading';
         this.wrapper = iDiv;
-        this.createContent()
+        this.updateContent()
         document.getElementById('overview').appendChild(iDiv);
     }
-    this.createContent = function(percentage){
+    this.updateContent = function(percentage){
         this.wrapper.innerHTML = "";
         var progress = document.createElement('div');
         progress.id = this.id;
@@ -42,8 +44,41 @@ function progressBar(){
         progress.className = 'progress';
         var indicater = document.createElement('div');
         indicater.style.marginLeft = "auto"
-        indicater.innerHTML = "Loading..."
+        if(percentage){
+            if(percentage > 20){
+                if(percentage > 50){
+                    if(percentage > 80){
+                        indicater.innerHTML = "Very Busy"
+                        progress.style.background = red
+                    }else{
+                        indicater.innerHTML = "Busy"
+                        progress.style.background = orange
+                    }
+                }else{
+                    indicater.innerHTML = "Rather Quiet"
+                    progress.style.background = darkGreen
+                }
+            }else{
+                indicater.innerHTML = "Very Quiet"
+                progress.style.background = green
+            }
+            var that = this
+            setTimeout(function() {
+                that.updatePercentage(percentage)
+            }, 50)
+ 
+        }else{
+            indicater.innerHTML = "Loading..."
+        }
         this.wrapper.appendChild(progress)
         this.wrapper.appendChild(indicater)
+        
+    }
+
+    this.updatePercentage = function(percentage){
+        var progress = document.getElementById(this.id)
+        if( progress && percentage){
+            progress.style.width = percentage + "%"
+        }
     }
 }
