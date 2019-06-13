1128d7b30e21a2722224fa81cf521cca657f4d2a
Henry Zhang <henryzhang9802@gmail.com>
2019-06-13 16:41:47 +0100

    Now can dynamically add div!

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ function typeWriter() {
     document.getElementById("sc").style.width="50%";
 }
 
+var studentCentre = new progressBar();
+studentCentre.create("sc")
 
+function progressBar(){
+    this.color = "";
+    this.id = "";
+    this.wrapper = null;
+    this.create = function(id){
+        var iDiv = document.createElement('div');
+        this.id = id;
+        iDiv.id = id;
+        iDiv.className = 'progress-wrapper';
+        this.wrapper = iDiv;
+        document.getElementById('overview').appendChild(iDiv);
+    }
+}
