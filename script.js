8b39e0b0be9d7db322af70084a9a46620962f203
Henry Zhang <henryzhang9802@gmail.com>
2019-06-13 16:51:13 +0100

    Can now load infos

--- script.js
+++ script.js
@@ -10,12 +10,26 @@ function progressBar(){
     this.color = "";
     this.id = "";
     this.wrapper = null;
-    this.create = function(id){
+    this.placeName = "";
+    this.create = function(id, name){
         var iDiv = document.createElement('div');
         this.id = id;
-        iDiv.id = id;
+        this.placeName = name;
         iDiv.className = 'progress-wrapper';
         this.wrapper = iDiv;
+        this.createContent()
         document.getElementById('overview').appendChild(iDiv);
+    }
+    this.createContent = function(percentage){
+        this.wrapper.innerHTML = "";
+        var progress = document.createElement('div');
+        progress.id = this.id;
+        progress.innerHTML = this.placeName;
+        progress.className = 'progress';
+        var indicater = document.createElement('div');
+        indicater.style.marginLeft = "auto"
+        indicater.innerHTML = "Loading"
+        this.wrapper.appendChild(progress)
+        this.wrapper.appendChild(indicater)
     }
 }
