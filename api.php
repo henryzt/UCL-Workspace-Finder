459dd9a9af576d794a2cdc4c454d8045be486966
Henry Zhang <henryzhang9802@gmail.com>
2019-06-14 11:49:56 +0100

    Remove uneccessary lines

--- api.php
+++ api.php
@@ -3,14 +3,8 @@ include('config.php');
 
 $url = 'https://uclapi.com/workspaces/sensors/summary?token=' . $token;
 
-/* Forward POST on to the API: */
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL, $url);
-if($type == "post"){
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
-}
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
